ced15e5fb5edca995918852b071ba77741bd766d
Mike Love <mikelove@users.noreply.github.com>
2024-08-15 13:36:17 -0400

    bedtools

--- bioc/make_ranges_and_BED.R
+++ bioc/make_ranges_and_BED.R
@@ -1,1 +1,12 @@
 library(EnsDb.Hsapiens.v86)
+library(ensembldb)
+txp <- transcripts(EnsDb.Hsapiens.v86)
+seqlevelsStyle(txp) <- "UCSC"
+
+library(plyranges)
+query <- data.frame(seqnames="chr1", start=100e6, width=2e6) |>
+  as_granges()
+
+txp |>
+  filter_by_overlaps(query) |>
+  write_bed("chr1_100Mb_v86_txps.bed")
